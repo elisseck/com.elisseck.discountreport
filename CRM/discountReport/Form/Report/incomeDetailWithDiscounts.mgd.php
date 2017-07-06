f4cdcfbdcfe2c7f6612ff0fe08fdd997ea2b1b8c
Eli Lisseck <lisseck@gmail.com>
2017-07-06 16:04:16 -0400

    initial commit

---
+++ CRM/discountReport/Form/Report/incomeDetailWithDiscounts.mgd.php
@@ -0,0 +1,21 @@
+<?php
+// This file declares a managed database record of type "ReportTemplate".
+// The record will be automatically inserted, updated, or deleted from the
+// database as appropriate. For more details, see "hook_civicrm_managed" at:
+// http://wiki.civicrm.org/confluence/display/CRMDOC42/Hook+Reference
+return array (
+  0 => 
+  array (
+    'name' => 'CRM_Discountreport_Form_Report_incomeDetailWithDiscounts',
+    'entity' => 'ReportTemplate',
+    'params' => 
+    array (
+      'version' => 3,
+      'label' => 'incomeDetailWithDiscounts',
+      'description' => 'incomeDetailWithDiscounts (com.elisseck.discountreport)',
+      'class_name' => 'CRM_Discountreport_Form_Report_incomeDetailWithDiscounts',
+      'report_url' => 'com.elisseck.discountreport/incomedetailwithdiscounts',
+      'component' => 'CiviEvent',
+    ),
+  ),
+);
---
+++ templates/CRM/discountReport/Form/Report/incomeDetailWithDiscounts.tpl
@@ -0,0 +1,1 @@
+{include file="CRM/Report/Form/Event/Income.tpl"}
